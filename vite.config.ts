283cb23a95dc30310dad54371d0087a0f00fe239
x0y14 <ya01ma08@gmail.com>
2026-01-24 21:50:12 +0900

    clean

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src',
   plugins: [preact()],
+  build: {
+    outDir: '../dist'
+  }
 })
