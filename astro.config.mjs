3cf541807806562d22361ee1f94c0ce992d0253b
x0y14 <ya01ma08@gmail.com>
2026-03-20 18:21:37 +0900

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import UnoCSS from "@unocss/astro"
 import markdoc from "@astrojs/markdoc"
 import unoConfig from "./uno.config.ts"
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [
     sparkioIntegration({ unoConfig }),
     UnoCSS({ injectReset: true }),
     markdoc(),
   ],
+
+  adapter: cloudflare({
+    prerenderEnvironment: "node",
+  }),
 })
